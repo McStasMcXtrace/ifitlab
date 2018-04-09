1a82d9aa08cbf2329157146e559869680dbd6fdd
Jakob Garde <jaga@fysik.dtu.dk>
2018-04-09 15:10:03 +0200

    update defaults to work uniformly accross release and debug servers

--- iflproj/iflproj/settings.py
+++ iflproj/iflproj/settings.py
@@ -118,5 +118,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/ifl-static/'
+STATIC_URL = '/ifl/ifl-static/'
 STATIC_ROOT = '/srv/mcweb/ifitlab/iflproj/static/'
