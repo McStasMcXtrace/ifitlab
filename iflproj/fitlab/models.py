3964dc54854968cdadef51155ecc434f1cce91b7
Jakob Garde <jaga@fysik.dtu.dk>
2018-07-10 13:26:24 +0200

    add 'created' field to server/worker messages, and remove undo stack and upload migrations

--- iflproj/fitlab/models.py
+++ iflproj/fitlab/models.py
@@ -9,12 +9,14 @@ class GraphDef(models.Model):
     graphdef_json = TextField()
 
 class GraphUiRequest(models.Model):
+    created = DateTimeField('created', default=timezone.now)
     username = CharField(max_length=200)
     gs_id = CharField(max_length=200)
     cmd = CharField(max_length=200, default="update_run")
     syncset = TextField(blank=True, null=True)
 
 class GraphReply(models.Model):
+    created = DateTimeField('created', default=timezone.now)
     reqid = CharField(max_length=200, unique=True)
     reply_json = TextField()
 
@@ -28,8 +30,6 @@ class GraphSession(models.Model):
     stashed_graphdef = TextField(blank=True)
     stashed_pickle = TextField(blank=True)
     stashed_matfile = CharField(max_length=200, default="")
-
-    stashed_undostack = TextField(blank=True) # this is mainly for transferring the undo stack from a previous browser as a gimmick
 
     # restore point / quick save
     quicksave_graphdef = TextField(blank=True)
